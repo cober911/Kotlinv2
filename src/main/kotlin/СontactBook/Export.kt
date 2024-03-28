47c32dcfec197f216c9e0dbdd3e5d43bd9257d8e
Vladimir <wortex2014@bk.ru>
2024-03-28 21:46:27 +0300

    dsl

--- src/main/kotlin/СontactBook/Export.kt
+++ src/main/kotlin/СontactBook/Export.kt
@@ -11,9 +11,9 @@ class Export: Command {
     }
     class Derived (b: Write) : Write by b
 
-    fun safeText(users:  MutableList<Person>) {
+    fun safeText(users: MutableList<Person>) {
+        val b = WriteContact()
         for (i in users) {
-            val b = WriteContact()
             Derived(b).writeName(i.getName())
             Derived(b).writePhone(i.getPhone())
             Derived(b).writeEmail(i.getMail())
@@ -22,20 +22,59 @@ class Export: Command {
         }
     }
 
-    fun run(users:  MutableList<Person>){
+    fun run(users: MutableList<Person>) {
         safeText(users)
-        var text ="[\n"
 
-        for (i in users){
-            val user = i.toString()
-            if (users.indexOf(i) != users.size-1) {
-                text = "$text$user,\n"
-            }else{
-                text = "$text$user\n]"
+        val jsonContent = json {
+            "users" to users.map {
+                json {
+                    "name" to it.getName()
+                    "phone" to it.getPhone()
+                    "email" to it.getMail()
+                }
             }
-        }
-        File("src/main/kotlin/UserDB/", "usersDB.json").writeText("")
-        File("src/main/kotlin/UserDB/", "usersDB.json").appendText(text)
+        }.toString()
+
+        File("src/main/kotlin/UserDB/", "usersDB.json").writeText(jsonContent)
+    }
+}
+
+// Класс для представления JSON объектов
+class JSONObject {
+    private val map = mutableMapOf<String, Any?>()
+
+    infix fun String.to(value: Any?) {
+        map[this] = value
     }
 
+    override fun toString(): String {
+        val jsonString = StringBuilder()
+        jsonString.append("{\n")
+        map.entries.forEachIndexed { index, entry ->
+            val (key, value) = entry
+            jsonString.append("\"$key\":")
+            when (value) {
+                is String -> jsonString.append("\"$value\"")
+                is Int, is Long, is Double, is Boolean -> jsonString.append(value)
+                is JSONObject -> jsonString.append(value.toString())
+                is List<*> -> {
+                    val jsonArray = value.joinToString(prefix = "[", postfix = "]", separator = ",")
+                    jsonString.append(jsonArray)
+                }
+            }
+            if (index < map.size - 1)
+                jsonString.append(",")
+            jsonString.append("\n")
+        }
+        jsonString.append("}")
+        return jsonString.toString()
+    }
 }
+
+// Функция для создания JSON объекта
+fun json(init: JSONObject.() -> Unit): JSONObject {
+    val jsonObject = JSONObject()
+    jsonObject.init()
+    return jsonObject
+}
+
